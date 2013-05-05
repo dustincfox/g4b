d584c60251beafd5aadaa6fe93a4bf680b27f66b
ajbraus <ajbraus@gmail.com>
2013-05-05 00:20:56 -0500

    blah

--- app/models/action.rb
+++
@@ -1,6 +0,0 @@
-class Action < ActiveRecord::Base
-  belongs_to :user
-  has_and_belongs_to_many :tags
-  has_many :comments, as: :commentable
-  attr_accessible :desc, :happening_on, :location, :title
-end
